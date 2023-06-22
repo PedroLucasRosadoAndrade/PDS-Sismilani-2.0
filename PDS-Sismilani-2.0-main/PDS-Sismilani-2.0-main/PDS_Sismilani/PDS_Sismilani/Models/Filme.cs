c648b86ffbb3047af8ee4f26e231d7327c61bd37
Ana Clara <83829829+anaclara93@users.noreply.github.com>
2023-06-21 22:50:36 -0300

    Add partes do  Filme
    obs: a AParte do DAO do filme não soube fazer.

---
+++ PDS-Sismilani-2.0-main/PDS-Sismilani-2.0-main/PDS_Sismilani/PDS_Sismilani/Models/Filme.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDS_Sismilani.Models
+{
+    internal class Filme
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string Valor { get; set; }
+
+        public string Codigo { get; set; }
+
+        public string Fornecedor { get; set; }
+
+        public string Unidade { get; set; }
+
+        public DateTime? DataFilme { get; set; }
+    }
+}
