574529102a377278c9d2c4f797f757e8dd723798
PedroLucasRosadoAndrade <126489915+PedroLucasRosadoAndrade@users.noreply.github.com>
2023-06-22 12:58:40 -0400

    ajuste de tela

--- PDS_Sismilani/PDS_Sismilani/Interfaces/IDAO.cs
+++ PDS_Sismilani/PDS_Sismilani/Interfaces/IDAO.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace PDS_Sismilani.Interfaces
 {
-    internal class IDAO
+    /// <summary>
+    ///     Interface (contrato) para classes DAO
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal class IDAO<T>
     {
+        /*void Insert(T t);
+
+        void Update(T t);
+
+        void Delete(T t);
+
+        List<T> List();
+
+        T GetById(int id);*/
     }
 }
--- PDS_Sismilani/PDS_Sismilani/Models/Funcionario.cs
+++ PDS_Sismilani/PDS_Sismilani/Models/Funcionario.cs
@@ -4,9 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace PDS_Sismilani.Models
 {
-    internal class Funcionario
+    internal class Funcionario 
     {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+        public DateTime? DataNascimento { get; set; }
+
+        public string Sexo { get; set; }
+
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+        public string Funcao { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+        public string RG { get; set; }
+
+        
     }
 }
