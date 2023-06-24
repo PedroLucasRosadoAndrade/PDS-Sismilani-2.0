319dba8a0d7a817ff924fc7d453c05cf34c30775
PedroLucasRosadoAndrade <126489915+PedroLucasRosadoAndrade@users.noreply.github.com>
2023-06-23 23:27:26 -0400

    Ajuste da tela Cliente

--- PDS_Sismilani/PDS_Sismilani/Models/Cliente.cs
+++ PDS_Sismilani/PDS_Sismilani/Models/Cliente.cs
@@ -8,5 +8,21 @@ namespace PDS_Sismilani.Models
 {
     internal class Cliente
     {
+        public string nome { get; set; }
+        public string rg { get; set; }
+        public string cidade { get; set; }
+        public string uf { get; set; }
+        public string telefone { get; set; }
+        public string email { get; set; }
+        public DateTime? dataNasc { get; set; }
+        public string cpf { get; set; }
+        public string rua { get; set; }
+        public string senha { get; set;}
+        public string sexo { get; set; }
+        public string bairro { get; set; }
+        public string id_log_fk { get; set; }
+        public string id_ing_fk { get; set; }
+        public string id_end_fk { get; set; }
+
     }
 }
