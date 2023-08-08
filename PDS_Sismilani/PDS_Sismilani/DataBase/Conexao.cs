7c744c60ba379b4a063c85dfdfabb3114462dc5d
PedroLucasRosadoAndrade <126489915+PedroLucasRosadoAndrade@users.noreply.github.com>
2023-08-08 14:16:50 -0400

    Teste de Conexao

--- PDS_Sismilani/PDS_Sismilani/DataBase/Conexao.cs
+++ PDS_Sismilani/PDS_Sismilani/DataBase/Conexao.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace PDS_Sismilani.DataBase
 {
-    internal class Conexao
-    {
+     class Conexao
+     {
+          string conexaoString = "server=localhost;database=bd_Sismilani;user=root;password=root;port=3306";
+            /*conexao = new MySqlConnection(conexaoString);
+            comando = conexao.CreateCommand();
 
-    }
+            conexao.Open();*/
+       
+     }
 }
