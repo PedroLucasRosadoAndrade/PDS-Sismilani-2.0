89326c20ea8c9177c2e610a850375686fa7f43b8
Ana Clara <83829829+anaclara93@users.noreply.github.com>
2023-10-01 10:55:10 -0300

    Criei o menu lateral na tela Cadastrar Clientes

--- PDS_Sismilani/PDS_Sismilani/Views/CadastrarCliente.xaml.cs
+++ PDS_Sismilani/PDS_Sismilani/Views/CadastrarCliente.xaml.cs
@@ -1,13 +1,6 @@
 ﻿using Org.BouncyCastle.Utilities.Collections;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -18,9 +11,6 @@ using System.Windows.Media.TextFormatting;
 
 namespace PDS_Sismilani.Views
 {
-    /// <summary>
-    /// Lógica interna para CadastrarCliente.xaml
-    /// </summary>
     public partial class CadastrarCliente : Window
     {
        MySqlConnection conexao;
@@ -32,6 +22,35 @@ namespace PDS_Sismilani.Views
             InitializeComponent();
             Conexao();
             //txtNome.Focus();        
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e) //esse
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+        private bool IsMaximized = false;
+        private void Border_MouseLeftButtonDown(object  sender, MouseButtonEventArgs e) // e esse trecho estão fazendo a responsividade da tela 
+        {
+            if (e.ClickCount == 2)
+            {
+                if (IsMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1080;
+                    this.Height = 720;
+
+                    IsMaximized = false;
+                }
+                else
+                {
+
+                    this.WindowState = WindowState.Maximized;
+
+                    IsMaximized = false;
+                }
+            }
         }
 
         private void Conexao()
@@ -47,103 +66,12 @@ namespace PDS_Sismilani.Views
         {
 
         }
-
-        private void btnSalvar_Click(object sender, RoutedEventArgs e)
-        {
-            try 
-            {
-                var dataNasc = dtPickerDataNascimento.SelectedDate;
-
-                bool _rdSexo1 = (bool)rdSexo1.IsChecked;
-                var _rdSexo2 = (bool)rdSexo2.IsChecked;
-
-
-
-                if (!(bool)rdSexo1.IsChecked && !(bool)rdSexo2.IsChecked)
-                {
-                    MessageBox.Show("Marque uma opção");
-                }
-                else
-                {
-
-                    var nome = txtNome.Text;
-                    var rg = TxtRG.Text;
-                    var cidade = txtCidade.Text;
-                    var uf = txtUF.Text;
-                    var telefone = txtTelefone.Text;
-                    var email = txtEmail.Text;
-                    var cep = txtCep.Text;
-                    var cpf = txtCpf.Text;
-                    var rua = TxtRua.Text;
-                    var senha = TxtSenha.Text;
-                    var sexo = "Feminino";
-                    //var bairro = txtBairro.Text;
-                   
-
-                    if ((bool)rdSexo1.IsChecked)
-                    {
-                        sexo = "Masculino";
-                    }
-
-
-                    string query = "INSERT INTO Funcionario (nome_cli, rg_cli, cidade_cli, uf_cli, telefone_cli, email_cli, cep_cli, data_nasc_cli, cpf_cli, rua_cli, senha_cli, sexo_cli) VALUES (@_nome, @_rg, @_cidade, @_uf, @_telefone, @_email, @_cep, @_dataNasc,  @_cpf, @_sexo, @_rua, @_senha, @_sexo_cli)";
-                    var comando = new MySqlCommand(query, conexao);
-
-                    comando.Parameters.AddWithValue("@_nome", nome);
-                    comando.Parameters.AddWithValue("@_rg", rg);
-                    comando.Parameters.AddWithValue("@_cidade", cidade);
-                    comando.Parameters.AddWithValue("@_uf", uf);
-                    comando.Parameters.AddWithValue("@_telefone", telefone);
-                    comando.Parameters.AddWithValue("@_email", email);
-                    comando.Parameters.AddWithValue("", cep);
-                    comando.Parameters.AddWithValue("@_dataNasc", dataNasc);
-                    comando.Parameters.AddWithValue("@_cpf", cpf);
-                    comando.Parameters.AddWithValue("@_rua", rua);
-                    comando.Parameters.AddWithValue("@_senha", senha);
-                    comando.Parameters.AddWithValue("@_sexo", sexo);
-                    //comando.Parameters.AddWithValue("@_bairro", bairro);
-                    
-
-                    comando.ExecuteNonQuery();
-                    txtNome.Clear();
-                    TxtRG.Clear();
-                    txtCidade.IsEnabled = false;
-                    txtUF.IsEnabled = false;
-                    txtTelefone.Clear();
-                    txtEmail.Clear();
-                    txtCep.IsEnabled = false;
-                    txtCpf.Clear();
-                    TxtRua.Clear();
-                    TxtSenha.Clear();
-                    //txtBairro.Clear();
-                    dtPickerDataNascimento.IsEnabled = false;
-                    rdSexo1.IsChecked = false;
-                    rdSexo2.IsChecked = false;
-                    txtNome.Focus();
-
-                    var opcao = MessageBox.Show("Salvo com sucesso!\n" +
-                        "Deseja realizar um novo cadastro?", "Informação",
-                        MessageBoxButton.YesNo, MessageBoxImage.Question);
-
-
-                    if (opcao == MessageBoxResult.Yes)
-                    {
-                        LimparImputs();
-                        MainWindow form = new MainWindow();
-                        form.ShowDialog();
-                    }
-                    else
-                    {
-                        this.Close();
-                    }
-                }
-
-            } catch { }
-        }
-
         private void btLimpar_Click(object sender, RoutedEventArgs e)
         {
 
         }
+
+        
+       
     }
 }
