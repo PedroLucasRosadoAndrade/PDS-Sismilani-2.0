232a6aeb04ba5b84907c742575b0eea26fdeece2
Pedro Lucas <rosadoandradepedrolucas@gmail.com>
2023-12-06 06:48:41 -0400

    ajuste de fornecedor

--- PDS_Sismilani/PDS_Sismilani/Models/FornecedorDAO.cs
+++ PDS_Sismilani/PDS_Sismilani/Models/FornecedorDAO.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using PDS_Sismilani.DataBase;
+using PDS_Sismilani.Helpers;
+using PDS_Sismilani.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,207 @@ using System.Threading.Tasks;
 
 namespace PDS_Sismilani.Models
 {
-    internal class FornecedorDAO
+    internal class FornecedorDAO : IDAO<Fornecedor>
     {
+        private static Conexao conn;
+
+        public FornecedorDAO()
+        {
+            conn = new Conexao();
+        }
+        public void Delete(Fornecedor t)
+        {
+            try
+            {
+                var query = conn.Query();
+                query.CommandText = "DELETE FROM Fornecedor WHERE id_for = @id";
+
+                query.Parameters.AddWithValue("@id", t.Id);
+
+                var result = query.ExecuteNonQuery();
+
+                if (result == 0)
+                    throw new Exception("Registro não removido da base de dados. Verifique e tente novamente.");
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public Fornecedor GetById(int id)
+        {
+            try
+            {
+                var query = conn.Query();
+                //query.CommandText = "SELECT * FROM funcionario " +
+                //                                "LEFT JOIN sexo ON cod_sex = cod_sex_fk " +
+                //                                "LEFT JOIN endereco ON cod_end = cod_end_fk " +
+                //                                "WHERE cod_func = @id";
+                query.CommandText = "SELECT * FROM forncedor";
+
+                query.Parameters.AddWithValue("@id", id);
+
+                MySqlDataReader reader = query.ExecuteReader();
+
+                if (!reader.HasRows)
+                    throw new Exception("Nenhum registro foi encontrado!");
+
+                var fornecedor = new Fornecedor();
+
+                while (reader.Read())
+                {
+
+                    fornecedor.Id = reader.GetInt32("id_for");
+                    fornecedor.Nome = reader.GetString("Nome_for");
+                    fornecedor.CNPJ = reader.GetString("CNPJ_for");
+                    fornecedor.tipo = reader.GetString("tipo_for");
+                    fornecedor.telefone = reader.GetString("telefone_for");
+                    fornecedor.historico = reader.GetString("historico_for");
+                    
+                }
+
+                return fornecedor;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conn.Query();
+            }
+        }
+
+        public void Insert(Fornecedor t)
+        {
+            try
+            {
+ 
+                var query = conn.Query();
+                query.CommandText = "INSERT INTO Fornecedor " +
+                    "(Nome_for, CNPJ_for, tipo_for, telefone_for, historico_for) " +
+                    "VALUES (@nome, @cnpj, @tipo, @telefone, @historico)";
+
+                //query.CommandText = "CALL inserir_funcionario(@nome, @datanasc, @cpf, @salario, @salario, @funcao, @email, @celular, @rg, @sexo)";
+
+                //query.Parameters.AddWithValue("id", t.Id);
+                query.Parameters.AddWithValue("@nome", t.Nome);
+                query.Parameters.AddWithValue("@cnpj", t.CNPJ); //"10/11/1990" -> "1990-11-10"
+                query.Parameters.AddWithValue("@tipo", t.tipo);
+                query.Parameters.AddWithValue("@telefone", t.telefone);
+                query.Parameters.AddWithValue("@historico", t.historico);
+                
+
+                var result = query.ExecuteNonQuery();
+
+                if (result == 0)
+                    throw new Exception("O registro não foi inserido. Verifique e tente novamente");
+
+
+                MySqlDataReader reader = query.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    if (reader.GetName(0).Equals("Alerta"))
+                    {
+                        throw new Exception(reader.GetString("Alerta"));
+                    }
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public List<Fornecedor> List()
+        {
+            try
+            {
+                List<Fornecedor> list = new List<Fornecedor>();
+
+                var query = conn.Query();
+                query.CommandText = "SELECT * FROM Fornecedor";
+
+                MySqlDataReader reader = query.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    list.Add(new Fornecedor()
+                    {
+                        Id = reader.GetInt32("id_for"),
+                        Nome = DAOHelper.GetString(reader, "Nome_for"),
+                        CNPJ = DAOHelper.GetString(reader, "CNPJ_for"),
+                        tipo = DAOHelper.GetString(reader, "tipo_for"),
+                        telefone = DAOHelper.GetString(reader, "telefone_for"),
+                        historico = DAOHelper.GetString(reader, "historico_for")
+
+
+                    });
+                }
+
+                return list;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void Update(Fornecedor t)
+        {
+            try
+            {
+                //long enderecoId = t.Endereco.Id;
+                //var endDAO = new EnderecoDAO();
+
+                //if (enderecoId > 0)
+                //    endDAO.Update(t.Endereco);
+                //else
+                //    enderecoId = endDAO.Insert(t.Endereco);
+
+                var query = conn.Query();
+                
+                query.CommandText = "UPDATE Fornecedor SET Nome_for = @nome, CNPJ_for = @cnpj, tipo_for = @tipo, telefone_for = @telefone, " +
+                    "historico_for = @historico WHERE id_for = @id";
+
+                query.Parameters.AddWithValue("@nome", t.Nome);
+                query.Parameters.AddWithValue("@cnpj", t.CNPJ); //"10/11/1990" -> "1990-11-10"
+                query.Parameters.AddWithValue("@tipo", t.tipo);
+                query.Parameters.AddWithValue("@telefone", t.telefone);
+                query.Parameters.AddWithValue("@historico", t.historico);
+                query.Parameters.AddWithValue("@id", t.Id);
+
+                var result = query.ExecuteNonQuery();
+
+                if (result == 0)
+                    throw new Exception("Atualização do registro não foi realizada.");
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
     }
 }
--- PDS_Sismilani/PDS_Sismilani/Views/CadastrarFornecedor.xaml.cs
+++ PDS_Sismilani/PDS_Sismilani/Views/CadastrarFornecedor.xaml.cs
@@ -24,100 +24,127 @@ namespace PDS_Sismilani.Views
     /// </summary>
     public partial class CadastrarFornecedor : Window
     {
-        MySqlConnection conexao;
-        MySqlCommand comando;
-        ObservableCollection<Fornecedor> fornecedors = new ObservableCollection<Fornecedor>(); // Coleção de clientes
+        private int _id;
+
+        private Fornecedor _fornecedor;
         public CadastrarFornecedor()
         {
             InitializeComponent();
+            Loaded += CadastrarFornecedor_Loaded;
         }
-        private void Conexao() // criando conexão
+        public CadastrarFornecedor(int id)
         {
-            string conexaoString = "server=localhost;database=cinemilani_bd;user=root;password=root;port=3360";
-            conexao = new MySqlConnection(conexaoString);
-            comando = conexao.CreateCommand();
+            _id = id;
+            InitializeComponent();
+            Loaded += CadastrarFornecedor_Loaded;
+        }
+        private void CadastrarFornecedor_Loaded(object sender, RoutedEventArgs e)
+        {
+            _fornecedor = new Fornecedor();
 
-            conexao.Open();
+            //LoadComboBox();
+
+            if (_id > 0)
+                FillForm();
+        }
+        private void btSalvar(object sender, RoutedEventArgs e)
+        {
+            _fornecedor.Id = _id;
+            _fornecedor.Nome = txtNome.Text;
+            _fornecedor.CNPJ = txtCnpj.Text;
+            _fornecedor.tipo = txtTipo.Text;
+            _fornecedor.telefone = txtTelefone.Text;
+            _fornecedor.historico = txtHistorico.Text;
+            
+
+            SaveData();
         }
 
-        private void btSalvar(object sender, RoutedEventArgs e)
+        private void SaveData()
         {
             try
             {
+               
+                    var dao = new FornecedorDAO();
+                    var text = "atualizado";
 
-                var nome = txtNome.Text;
-                var cnpj = txtCnpj.Text;
-                var telefone = txtTelefone.Text;
-                var tipo = txtTipo.Text;
-                var historico = txtHistorico.Text;
-
-                using (MySqlConnection conexao = new MySqlConnection("server=localhost;database=cinemilani_bd;user=root;password=root;port=3360"))
-                {
-                    conexao.Open();
-
-                    string query = "INSERT INTO fornecedor (Nome_for, CNPJ_for, tipo_for, telefone_for, Historico_for) " +
-                                   "VALUES (@_nome, @_CNPJ, @_tipo, @_telefone, @_historico)";
-                    using (MySqlCommand comando = new MySqlCommand(query, conexao))
+                    if (_fornecedor.Id == 0)
                     {
-                        comando.Parameters.AddWithValue("@_Nome", nome);
-                        comando.Parameters.AddWithValue("@_cnpj", cnpj);
-                        comando.Parameters.AddWithValue("@_telefone", telefone);
-                        comando.Parameters.AddWithValue("@_tipo", tipo);
-                        comando.Parameters.AddWithValue("@_historico", historico);
-
-
-                        comando.ExecuteNonQuery();
+                        dao.Insert(_fornecedor);
+                        text = "adicionado";
                     }
-                }
+                    else
+                        dao.Update(_fornecedor);
 
-                txtNome.Clear();
-                txtCnpj.Clear();
-                txtTelefone.Clear();
-                txtTipo.Clear();
-                txtHistorico.Clear();
-
-                txtNome.Focus();
-
-                var opcao = MessageBox.Show("Fornecedor cadastrado com sucesso!\n" +
-                    "Deseja realizar um novo cadastro?", "Informação",
-                    MessageBoxButton.YesNo, MessageBoxImage.Question);
-
-
-                if (opcao == MessageBoxResult.Yes)
-                {
-
-                    txtNome.Clear();
-                    txtCnpj.Clear();
-                    txtTelefone.Clear();
-                    txtTipo.Clear();
-                    txtHistorico.Clear();
-
-                    txtNome.Focus();
-                }
-                else
-                {
-                    this.Close();
-                }
+                    MessageBox.Show($"O Fornecedor foi {text} com sucesso!", "Sucesso", MessageBoxButton.OK, MessageBoxImage.Information);
+                    CloseFormVerify();
             }
-
             catch (Exception ex)
             {
-                MessageBox.Show($"Ocorreram erros ao tentar salvar os dados!\n" +
-                    $"Contate o suporte do sistema. (CAD 25)\n\n{ex.Message}");
-
-                txtNome.Clear();
-                txtCnpj.Clear();
-                txtTelefone.Clear();
-                txtTipo.Clear();
-                txtHistorico.Clear();
+                MessageBox.Show(ex.Message, "Não Executado", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-        } 
-
-            private void btVoltar(object sender, RoutedEventArgs e)
+        }
+        private void FillForm()
+        {
+            try
             {
-            this.Close();
+                var dao = new FornecedorDAO();
+                _fornecedor = dao.GetById(_id);
+                txtId.Text = _fornecedor.Id.ToString();
+                txtNome.Text = _fornecedor.Nome;
+                txtCnpj.Text = _fornecedor.CNPJ;
+                txtTipo.Text = _fornecedor.tipo;
+                txtTelefone.Text = _fornecedor.telefone;
+                txtHistorico.Text = _fornecedor.historico;
+                
+                //if (_funcionario.Sexo != null)
+                //    comboBoxSexo.SelectedValue = _funcionario.Sexo.Id;
+
+                //if (_funcionario.Endereco != null)
+                //{
+                //    txtRua.Text = _funcionario.Endereco.Rua;
+                //    txtNumero.Text = _funcionario.Endereco.Numero.ToString();
+                //    txtBairro.Text = _funcionario.Endereco.Bairro;
+                //    txtCidade.Text = _funcionario.Endereco.Cidade;
+
+                //    comboBoxEstado.SelectedValue = _funcionario.Endereco.Estado;
+                //}
 
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exceção", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void CloseFormVerify()
+        {
+            if (_fornecedor.Id == 0)
+            {
+                var result = MessageBox.Show("Deseja continuar adicionando Fornecedores?", "Continuar?", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                if (result == MessageBoxResult.No)
+                    this.Close();
+                else
+                    ClearInputs();
+            }
+            else
+                this.Close();
+        }
+        private void ClearInputs()
+        {
+
+            txtNome.Text = "";
+            txtCnpj.Text = "";
+            txtTipo.Text = "";
+            txtTelefone.Text = "";
+            txtHistorico.Text = "";
+           
+        }
+        private void btVoltar(object sender, RoutedEventArgs e)
+        {
+          this.Close();
+
+        }
 
         private void txtEstado_TextChanged(object sender, TextChangedEventArgs e)
         {
